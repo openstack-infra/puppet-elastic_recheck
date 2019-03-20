678e461ca755d090eb58475bbd50d78524486a77
Clark Boylan <clark.boylan@gmail.com>
2019-03-20 15:16:14 -0700

    flock lockfile path comes before the command to lock
    When I added the timeout I didn't move the lock file path properly. Fix
    this by putting the lock file path before the timeout command to flock.
    
    Change-Id: Iacf8d55f9cfe5b2c92569929bbdadebbe2f8de09

--- files/er_safe_run.sh
+++ files/er_safe_run.sh
@@ -2,4 +2,4 @@
 
 # Timeout after 4 hours as we've seen this script occasionally fail to
 # exit after running for days.
-flock -w 3600 timeout -k 60 14400 /var/lib/elastic-recheck/er_safe_run.lock $@
+flock -w 3600 /var/lib/elastic-recheck/er_safe_run.lock timeout -k 60 14400 $@
